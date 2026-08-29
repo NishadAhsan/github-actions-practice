18f7f531c8e98fb88e36b6e3224f832c8db59963
Nazmul Ahsan <nishadahsan@gmail.com>
2026-08-30 03:50:41 +0600

    test: introduce lint formatting violation

--- src/GitHubActionsPractice.Api/Models/TodoItem.cs
+++ src/GitHubActionsPractice.Api/Models/TodoItem.cs
@@ -2,7 +2,7 @@ namespace GitHubActionsPractice.Api.Models;
 
 public class TodoItem
 {
-    public int Id { get; set; }
+    public int Id {get; set; }
 
     public required string Title { get; set; }
 
